103a84cfd40e576624a9497df890643133b12066
Eugene Zemlyanko <slayer0007@mail.ru>
2023-06-27 00:09:30 +0300

    inition

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include"phoneDB.h"
+
+int main()
+{
+	//setlocale(LC_ALL, "ru-RU");
+	//system("chcp 1251");
+
+	phoneDB db("localhost", "5432", "phone", "postgres", "392260");
+	db.create_tbls();
+
+
+
+
+
+	return 0;
+}
